d3636b16be928641453e9571736d562386a0fcf7
Michael Brantley <brantleyrecs@gmail.com>
2023-04-30 17:17:11 -0500

    started the expel button and worked on the random house function

--- main.js
+++ main.js
@@ -11,7 +11,16 @@ const students = [
     name: "Ron Edward Beansley",
     house: "Gryffindor",
     imgURL: "https://static.boredpanda.com/blog/wp-content/uploads/2020/04/Bean-Sheeran-5e980d96dee76__880.jpg"
-  }
+  },
+]
+
+const expelledStudents = [
+  {
+    id: 3,
+    name: "Lord Valdibean",
+    house: "Slytherin",
+    imgURL: "https://sadanduseless.b-cdn.net/wp-content/uploads/2019/07/funny-mr-bean13.jpg"
+  },
 ]
 
 const renderToDom = (divId, htmlToRender) => {
@@ -22,26 +31,78 @@ const renderToDom = (divId, htmlToRender) => {
 
 const cardsOnDom = (array) => {
   let domString = "";
+    for (const student of array) {
+       domString += `<div class="card" style="width: 18rem;">
+  <img src="${student.imgURL}" class="card-img-top" alt="${student.name}">
+  <div class="card-body">
+    <h5 class="card-title">${student.name}</h5>
+    <p class="card-text">House: ${student.house}</p>
+    <button id="expelButton--${student.id}" type="button" class="btn btn-danger">Expel!</button>
+  </div>
+</div>`
+   }
+   renderToDom('#app', domString)  
+   document.querySelector("#app").addEventListener("click", expelStudent)
+} 
+
+const expelledOnDom = (array) => {
+  let domString = "";
   for (const student of array) {
     domString += `<div class="card" style="width: 18rem;">
   <img src="${student.imgURL}" class="card-img-top" alt="${student.name}">
   <div class="card-body">
     <h5 class="card-title">${student.name}</h5>
-    <p class="card-text">House: ${student.house}</p>
+    <p class="card-text">Has gone to the dark side!</p>
   </div>
 </div>`
   }
+  renderToDom("#bad", domString)
+}
 
-  renderToDom('#app', domString)
-  console.log(domString);
-} 
+const expelStudent = (event) => {
+  if (event.target.id.includes("expelButton")) {
+    const [, studentId] = event.target.id.split("--")
+    const studentIndex = students.findIndex(
+      (student) => Number(studentId) === student.id
+    )
+    const expelledStudent = students.splice(studentIndex, 1)
+    expelledStudents.push(expelledStudent)
+
+    expelledOnDom(expelledStudent)
+    cardsOnDom(students)
+    console.log(expelledStudents)
+  }
+}
 
 
 const allStudents = document.querySelector('#all-students')
+const gryffindorButton = document.querySelector('#gryffindor')
+const hufflepuffButton = document.querySelector('#hufflepuff')
+const ravenclawButton = document.querySelector('#ravenclaw')
+const slytherinButton = document.querySelector('#slytherin')
+
+const filter = (array, houseString) => {
+  const houseArray = []
+
+  for (const student of array) {
+    if (student.house === houseString) {
+      houseArray.push(student)
+    }
+  }
+
+  return houseArray
+}
+
 
 
 allStudents.addEventListener('click', () => {
   cardsOnDom(students)
+  expelledOnDom(expelledStudents)
+})
+
+gryffindorButton.addEventListener('click', () => {
+  const gryffStudents = filter(students, 'gryffindor')
+  cardsOnDom(gryffStudents)
 })
 
 const form = document.querySelector('form')
@@ -49,10 +110,35 @@ const form = document.querySelector('form')
 const newStudent = (e) => {
   e.preventDefault()
 
+  const randNum = Math.floor(Math.random() * 4)
+
+  const randomHouse = students[randNum]
+
+  switch(randNum) {
+    case 0:
+      house = 'Gryffindor';
+      break;
+    case 1:
+      house =  'Hufflepuff';
+      break;
+    case 2: 
+      house =  'Ravenclaw';
+      break;
+    case 3:
+      house =  'Slytherin';
+      break;
+    default:
+      house =  'No house for you';
+  };
+
+  console.log(randNum);
+
+  
+
   const newStudentObj = {
-    id: students.length + 1,
+    id: students.length + 2,
     name: document.querySelector("#name").value,
-    // house: document.querySelector("#house").value,
+    house: randomHouse.house,
     imgURL: document.querySelector("#url").value,
   }
 
